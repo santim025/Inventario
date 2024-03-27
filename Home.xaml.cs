20cb4c40dbdf03bf28aa09a1c30452e9d86913a6
Santiago Cañon <santiago.canon@non.se.com>
2024-03-27 01:41:56 -0500

    FinishHomeView

---
+++ Home.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Iluminacion
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Window
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btn_cerrar(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private void Btn_minimizar(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Maximized;
+        }
+
+        private void PMLBDown(object sender, MouseButtonEventArgs e)
+        {
+            BtnShowHide.IsChecked = false;
+        }
+
+        private void TBShow(object sender, RoutedEventArgs e)
+        {
+            GridContent.Opacity = 0.5;
+        }
+
+        private void TBHide(object sender, RoutedEventArgs e)
+        {
+            GridContent.Opacity = 1;
+        }
+    }
+}
